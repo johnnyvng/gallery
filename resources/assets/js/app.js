d5db2bd8e5d76bfe6dc23d0914fcc2990726468a
Johnny <nguyen.johnny@live.com>
2016-04-11 23:13:51 -0400

    1. Setting up the app and routes
    SPA Laravel & AngularJS

---
+++ resources/assets/js/app.js
@@ -0,0 +1,21 @@
+/*This is the main file where angular is defined*/
+var myApp = angular.module('myApp', ['ngRoute', 'ngCookies']);
+
+myApp.config(['$routeProvider', '$locationProvider', 
+	function ($routeProvider, $locationProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl : 'templates/users/login.html',
+			controller : 'userController'
+		})
+		.when('/dashboard', {
+			templateUrl : 'templates/users/dashboard.html',
+			controller : 'userController'
+		})
+		.when('/logout', {
+			templateUrl : 'templates/users/logout.html',
+			controller : 'userController'
+		})
+		.otherwise({ redirectTo: '/' })
+	}
+]);
---
+++ resources/views/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html ng-app="myApp">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Gallery Application in Angular JS</title>
+	<link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+</head>
+<body>
+	<div class="container">
+		<div ng-view></div>
+	</div>
+	<script src="{{asset('bower_components/angular/angular.min.js')}}" type="text/javascript" charset="utf-8"></script>
+	<script src="{{asset('bower_components/angular-route/angular-route.min.js')}}" type="text/javascript" charset="utf-8"></script>
+	<script src="{{asset('bower_components/angular-cookies/angular-cookies.min.js')}}" type="text/javascript" charset="utf-8"></script>
+	<script src="{{asset('js/app.js')}}" type="text/javascript" charset="utf-8"></script>
+	<script src="{{asset('js/controllers.js')}}" type="text/javascript" charset="utf-8"></script>
+	
+</body>
+</html>
